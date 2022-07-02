16825d726860e09392edf7442bf711cb8b9df2fe
Selinda-C <selindachapman@icloud.com>
2022-07-02 14:02:15 -0700

    WIP attempting to make results log properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // business logic
 function count(userInput){
   let numberArray = []
-  for (let index = 0; index <= userInput; index +=1){
-    if (userInput.includes(3)){
+  for (let index = 0; index <= userInput; index ++){
+    if (userInput[index].includes(3)){
       numberArray.push("Won't you be my neighbor?")
-    } else if (userInput.includes(2)) {
-      numberArray.push("beep")
-    } else if (userInput.includes(1)){
+    } else if (userInput[index].includes(2)) {
+      numberArray.push("boop")
+    } else if (userInput[index].includes(1)){
       numberArray.push("beep")
     } else {
       numberArray.push(userInput[index])
@@ -14,11 +14,13 @@ function count(userInput){
   }
   return numberArray
 }
-let form = document.querySelector("form")
-form.addEventListener("submit" function(event){
-  event.preventDefault
-  const userInput = document.querySelector
-  count(userInput)
-})
-
+window.addEventListener("load", function(){
+  let form = document.querySelector("form")
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const userInput = document.querySelector("input").value;
+    const input = parseInt(userInput);
+    console.log(count(input));
+  });
+});
 // UI logic
