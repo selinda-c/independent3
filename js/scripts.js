c25a5da04f4933ff6dfa89ec0f232e4a11b08efc
Selinda-C <selindachapman@icloud.com>
2022-07-02 13:49:41 -0700

    add count function and submit event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 // business logic
-
-
-
-
+function count(userInput){
+  let numberArray = []
+  for (let index = 0; index <= userInput; index +=1){
+    if (userInput.includes(3)){
+      numberArray.push("Won't you be my neighbor?")
+    } else if (userInput.includes(2)) {
+      numberArray.push("beep")
+    } else if (userInput.includes(1)){
+      numberArray.push("beep")
+    } else {
+      numberArray.push(userInput[index])
+    }
+  }
+  return numberArray
+}
+let form = document.querySelector("form")
+form.addEventListener("submit" function(event){
+  event.preventDefault
+  const userInput = document.querySelector
+  count(userInput)
+})
 
 // UI logic
---
+++ readme.md
@@ -0,0 +1,6 @@
+describe returnResults()
+test: "it should return "beep" if a user inputs a number that includes 1"
+code: 
+const input = 1
+returnResults(input);
+expected output: "beep"
