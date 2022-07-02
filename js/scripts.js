789a7953a9d4edc814c457ce544b9c1c2e87920d
Selinda-C <selindachapman@icloud.com>
2022-07-02 14:47:34 -0700

    add additional function to reset button

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,18 @@ function count(userInput){
 // UI 
 window.addEventListener("load", function(){
   let form = document.querySelector("form")
+  resetBtn.style.display = "none";
   form.addEventListener("submit", function(event){
     event.preventDefault();
+    resetBtn.removeAttribute("style");
     const userInput = document.querySelector("input").value;
     const output = document.getElementById("output")
     output.append(count(userInput));
   });
   resetBtn.addEventListener("click", function(){
+    const input = document.querySelector("input");
     output.innerText = null;
+    input.value = null;
+    resetBtn.style.display = "none";
   })
 });
